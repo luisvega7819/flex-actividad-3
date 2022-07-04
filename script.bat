1f788b6d6af58edcd318de549b154dfbde3fa081
Luis Vega <luis.vega7819@hotmail.com>
2022-07-05 00:26:05 +0200

    codigo script.bat completo

--- script.bat
+++ script.bat
@@ -1,5 +1,13 @@
 @echo off
-flex LexicoX.l
+c:\GnuWin32\bin\flex LexicoX.l
 echo "Flex ha compilado la especificacion lexica del archivo Lexico1.l (Se crea un archivo lex.yy.c)"
 pause
-gcc.exe
+c:\MinGW\bin\gcc.exe lex.yy.c -o Compilado.exe
+echo "Finalizado la ejecución del compilador de C sobre el archivo lex.yy.c (Se creo un archivo Compilado.exe)"
+pause
+Compilado.exe Ejemplo.txt
+echo "Se ejecuto el analizador lexico sobre el archivo Ejemplo.txt"
+del lex.yy.c
+del Compilado.exe
+echo "Se borraron los archivos generados en todo este proceso, finalizado..."
+pause
